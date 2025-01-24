f5ef97a1e8f6b9aa2f5a3ffd6efab3411611e99c
teoaar <teoaar@elev.ntg.no>
2025-01-24 08:11:02 +0100

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,14 @@
-radio.setGroup(18)
+radio.onReceivedNumber(function (receivedNumber) {
+    basic.showNumber(receivedNumber)
+})
+radio.setGroup(3)
 basic.forever(function () {
     radio.sendNumber(1)
-    music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
     radio.sendNumber(2)
+    radio.sendNumber(3)
+    radio.sendNumber(4)
+    radio.sendNumber(5)
+    radio.sendNumber(6)
+    radio.sendNumber(7)
+    radio.sendNumber(8)
 })
